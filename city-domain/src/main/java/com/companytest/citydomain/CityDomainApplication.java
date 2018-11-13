2d435b42602da474b81a937ffb0c5dba259670a3
Cezar Augusto <cezar804@gmail.com>
2018-11-13 02:23:31 -0200

    Initial commit

---
+++ city-domain/src/main/java/com/companytest/citydomain/CityDomainApplication.java
@@ -0,0 +1,12 @@
+package com.companytest.citydomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityDomainApplication.class, args);
+	}
+}
---
+++ city-domain/src/test/java/com/companytest/citydomain/CityDomainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.companytest.citydomain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CityDomainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
