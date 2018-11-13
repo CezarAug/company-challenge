377e38b5decba95a327a6b87387665cee84e2fe0
Cezar Augusto <cezar804@gmail.com>
2018-11-13 04:41:51 -0200

    Adding eureka service discovery

--- city-domain/src/main/java/com/companytest/citydomain/CityDomainApplication.java
+++ city-domain/src/main/java/com/companytest/citydomain/CityDomainApplication.java
@@ -2,8 +2,10 @@ package com.companytest.citydomain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CityDomainApplication {
 
 	public static void main(String[] args) {
